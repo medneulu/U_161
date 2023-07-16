1b7a30aea412284f81d4e415ef4b15ec3d0b8486
zehramert <135542044+zehramert@users.noreply.github.com>
2023-07-16 16:09:47 +0300

    level2

---
+++ Assets/Script/PlatformController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    public Transform PosA, PosB;
+    public int Speed;
+    Vector2 targetPos; 
+    void Start()
+    {
+        targetPos = PosB.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, PosA.position) < 1f) targetPos = PosB.position;
+        if (Vector2.Distance(transform.position, PosB.position) < 1f) targetPos = PosA.position;
+
+        transform.position = Vector2.MoveTowards(transform.position,targetPos,Speed*Time.deltaTime);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            collision.transform.SetParent(this.transform);
+        }
+
+    }
+    private void OnTriggerxit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+        
+    }
+}
